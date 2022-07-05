e9d41443e9151375208357bbec7059f071b70979
BringFeel <69062690+BringFeel@users.noreply.github.com>
2022-07-04 22:13:16 -0300

    Version 2.0

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+PORT: '8080',
+HTTPS_PORT: '4545',
+USE_HTTPS: 'false',
+USE_HTTP: 'true'
+
+}
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+// Server
 var express = require('express');
 var app = express();
+var https = require('https');
 const chalk = require("chalk");
-require("dotenv").config();
-const port = process.env.PORT || 80
 var figlet = require('figlet');
 var errorHandler = require('express-error-handler');
 var fs = require('fs');
 var files = fs.readdirSync(__dirname + '/WebFiles');
+
+
+//Config
+this.config = require('./config.js');
+const port = this.config.PORT || 80
+const httpsport = this.config.HTTPS_PORT || 443
+const usehttps = this.config.USE_HTTPS || 'false';
+const usehttp = this.config.USE_HTTP || 'false';
 
   handler = errorHandler({
     static: {
@@ -14,25 +22,51 @@ var files = fs.readdirSync(__dirname + '/WebFiles');
     }
   });
 
+  if (usehttps === 'true') {
+    var httpsenabled = (chalk.green('ACTIVE'));
+  } else {
+    var httpsenabled = (chalk.red('INACTIVE'));
+  }
+
+  if (usehttp === 'true') {
+    var httpenabled = (chalk.green('ACTIVE'));
+  } else {
+    var httpenabled = (chalk.red('INACTIVE'));
+  }
+
+  console.log(chalk.magenta('----------------'), chalk.green('Web Server'), chalk.magenta('----------------'));
+  console.log()
+  console.log(chalk.cyan(figlet.textSync('Solari', {
+      font: 'ANSI Shadow',
+  })));
+  console.log(chalk.magenta('--------------------------------------------'));
+  console.log('Discord: https://discord.gg/t5sMubXFeX');
+  console.log('Website: https://bringfeel.com');
+  console.log('GitHub: https://github.com/BringFeel/solari');
+  console.log(chalk.magenta('-------------'), chalk.green(' Files/Folders '), chalk.magenta('--------------'));
+  console.log(files)
+  console.log(chalk.magenta('------------------'), chalk.green('Status'), chalk.magenta('------------------'));
+  console.log(`HTTP ${httpenabled} | PORT ${port}`)
+  console.log(`HTTPS ${httpsenabled} | PORT ${httpsport}`)
+  console.log(chalk.magenta('----------------'), chalk.green('Web Server'), chalk.magenta('----------------'));
+
+  if (usehttp === 'true') {
 app.use(express.static(__dirname + '/WebFiles'));
 
 app.use( errorHandler.httpError(404) );
 app.use( handler );
 
 app.listen(port, () => {
-    console.log(chalk.magenta('----------------'), chalk.green('Web Server'), chalk.magenta('----------------'));
-    console.log()
-    console.log(chalk.cyan(figlet.textSync('Solari', {
-        font: 'ANSI Shadow',
-    })));
-    console.log(chalk.magenta('--------------------------------------------'));
-    console.log('Discord: https://discord.gg/t5sMubXFeX');
-    console.log('Website: https://bringfeel.com');
-    console.log('GitHub: https://github.com/BringFeel/solari');
-    console.log(chalk.magenta('-------------'), chalk.green(' Files/Folders '), chalk.magenta('--------------'));
-    console.log(files)
-    console.log(chalk.magenta('----------------'), chalk.green('Web Server'), chalk.magenta('----------------'));
-    console.log(chalk.blue.bgRed.bold(`Server is running on http://0.0.0.0:${port}`))
+    console.log(chalk.blue.bgRed.bold(`Server http is running on http://0.0.0.0:${port}`))
 })
+  }
 
-//Tremendo Spam papá 
+if (usehttps === 'true') {
+  https.createServer({
+    cert: fs.readFileSync(__dirname + '/certificates/certificate.crt'),
+    key: fs.readFileSync(__dirname + '/certificates/certificate.key')
+  },app).listen(httpsport, function(){
+    console.log(chalk.blue.bgRed.bold(`Server https is running on http://0.0.0.0:${httpsport}`));
+  });
+  app.use(express.static(__dirname + '/WebFiles'));
+}
